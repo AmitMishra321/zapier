af5e4f1eadfe3559295c85aed3fb2882ad4fd69c
Deepak Patankar <deepakpatankar7227@gmail.com>
2024-12-22 00:57:21 +0530

    project setup Init & hooks added

---
+++ apps/hooks/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+import prisma from "@repo/db/client";
+
+app.use(express.json());
+
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  // password logic using userId
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
+
+  // store in db a new trigger
+  await prisma.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+    res.json({
+        message: "Webhook received"
+    })
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
